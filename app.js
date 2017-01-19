25d5337f8c773c506ddf8b7458dd55dc4204da53
Bruno Giovanini <bsgiovanini@gmail.com>
2017-01-19 21:04:27 -0200

    drawing

--- app.js
+++ app.js
@@ -47,6 +47,7 @@ window.onload = () => {
 		}
 
 		const pCWQuickUnion = new PCWQuickUnion(n+2);
+		const pCWQuickUnionBS = new PCWQuickUnion(n+2, false);
 
 		let percolated = false;
 
@@ -54,6 +55,7 @@ window.onload = () => {
 			
 			const sorted = getRandomInt(1, n+1);
 			const opened = pCWQuickUnion.connectNeighbors(sorted);
+			pCWQuickUnionBS.connectNeighbors(sorted);
 			const percolates = pCWQuickUnion.percolates();		
 
 			if(opened && !percolated) {
@@ -65,7 +67,7 @@ window.onload = () => {
 			if (percolated) {
 
 				for(let i = 1; i <= n; i++) {
-					if (pCWQuickUnion.isConnected(i, sorted)) {
+					if (pCWQuickUnionBS.isConnected(i, sorted)) {
 						const nodeFull = document.getElementById(i);
 						nodeFull.classList.add("grid-node-full");
 					}
